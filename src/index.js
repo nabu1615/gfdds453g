cd1f54620287492ea16f901ba99cfd8078b5d619
Javier Vargas <javier.vargas@globant.com>
2017-09-04 17:26:03 -0500

    challenge solution

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory, IndexRedirect} from 'react-router'
-import {App, Page1, Page2, NotFound} from './App';
+import { render } from 'react-dom';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router'
+import {Page1, Page2, NotFound, Template} from './App';
 import './index.css';
 
 
-// Implementa las rutas aca
-
-ReactDOM.render(
-  <App />, // Es posible que tengas que hacer un cambio en esta linea.
-  document.getElementById('root')
-);
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={Template}>
+    	<IndexRoute component={Page1}/>
+	    <Route path="/page1" component={Page1}/>
+	    <Route path="/page2" component={Page2}/>
+    </Route>
+	<Route path="*" component={NotFound}/>
+  </Router>
+), document.getElementById('root'))
